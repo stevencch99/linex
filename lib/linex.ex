931f6de6136f6e65b77389c7e7ffada8961c38b5
Steven C <stevencch99@gmail.com>
2023-03-14 17:55:19 +0800

    Init commit

---
+++ lib/linex.ex
@@ -0,0 +1,18 @@
+defmodule Linex do
+  @moduledoc """
+  Documentation for `Linex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Linex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/linex_test.exs
@@ -0,0 +1,8 @@
+defmodule LinexTest do
+  use ExUnit.Case
+  doctest Linex
+
+  test "greets the world" do
+    assert Linex.hello() == :world
+  end
+end
