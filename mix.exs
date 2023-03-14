1d2b3fbc2acc5d4ee00420210fc2a2560da44bba
Steven C <stevencch99@gmail.com>
2023-03-14 18:17:28 +0800

    Adding metadata to mix.exs

--- mix.exs
+++ mix.exs
@@ -7,23 +7,33 @@ defmodule Linex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      description: description(),
       deps: deps(),
-      name: "Linex"
+      name: "LINEx",
+      package: package(),
+      source_url: "git@github.com:stevencch99/linex.git"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
+    []
+  end
+
+  def description() do
+    "The LINE API wrapper"
+  end
+
+  def package() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/stevencch99/linex"},
+      maintainers: ["stevencch99"]
     ]
   end
 end
